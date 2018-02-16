9f9121023835d8b4243faf1b0973d7b4e4aab57b
Peter MOUEZA <mouezapeter@gmail.com>
2018-02-16 22:43:33 +0100

    Error starting ApplicationContext.
    To display the auto-configuration report re-run your application with
    'debug' enabled.

---
+++ SpringInitializr-MVC-Hibernate-WebJPA/src/main/java/homniserv/moueza/SpringInitializrMVCHibernateWebJPA/PersonBean.java
@@ -0,0 +1,25 @@
+package homniserv.moueza.SpringInitializrMVCHibernateWebJPA;
+
+public class PersonBean {
+String nom;
+String prenom;
+int anneeNaissance;
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public int getAnneeNaissance() {
+	return anneeNaissance;
+}
+public void setAnneeNaissance(int anneeNaissance) {
+	this.anneeNaissance = anneeNaissance;
+}
+}
---
+++ SpringInitializr-MVC-Hibernate-WebJPA/src/main/java/homniserv/moueza/SpringInitializrMVCHibernateWebJPA/SpringInitializrMvcHibernateWebJpaApplication.java
@@ -0,0 +1,12 @@
+package homniserv.moueza.SpringInitializrMVCHibernateWebJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrMvcHibernateWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrMvcHibernateWebJpaApplication.class, args);
+	}
+}
